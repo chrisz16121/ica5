094d580d2419bb2d99face0475eb07f304b1e8bd
chrisz16121 <ccszt2@mail.missouri.edu>
2016-10-28 12:45:34 -0500

    BaseSig can be built and printed..

--- ica5.cpp
+++ ica5.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <cstring>
+#include <stdio.h>
+#include <stdlib.h>
 
 using namespace std;
-
+int numObjects = 0;
 // --------- BaseSig class and methods ------------------------------
 class BaseSig{
 	private:
@@ -11,44 +13,53 @@ class BaseSig{
 	
 	protected:	// accessible by derived classes, not by other users.
 		int length;
-		int *raw_data;
-		
+		double *raw_data;
+		int maxVal;
 	public:
 		BaseSig();		// default constructor.
 		//BaseSig(int L);	// parametric constructor
 		~BaseSig();		// destructor
-		int getLength() { return length; };
-		int getRawValue(int pos);
-		static int numObjects;	// static, only one member for the entire hierarchy
+		//int getLength() { return length; };
+		//int getRawValue(int pos);
+		//static int numObjects;	// static, only one member for the entire hierarchy
 		virtual void printInfo();
 };
-
+void BaseSig::printInfo(){
+	int i;
+	cout << "Length is: " << length << "\nMaximum value is: " << maxVal << endl;
+}
 BaseSig::BaseSig(){
 	int i =0;
 	char fileString[50];
 	sprintf(fileString,"Raw_data_01.txt");//this is the file that i open by default 
 	FILE* fp = fopen(fileString,"r");//opening file
-	if(fp == NULL)//from this point on, the constructors are identical
+	if(fp == NULL)
 	{
 		cout << "Could not open the file, terminating\n" << endl;
 		return;
 	}
-	fscanf(fp,"%d %lf",&numEl,&maxVal);
+	fscanf(fp,"%d %d",&length,&maxVal);
 	raw_data = new double[length];
 	double* arrStart = raw_data;
-	while(i < numEl)
+	while(i < length)
 	{
 		int temp;
 		fscanf(fp,"%d\n",&temp); 
-		*array = temp;
-		array++;
+		*raw_data = temp;
+		raw_data++;
 		i++;
 	}
+	raw_data = arrStart;
 	numObjects++;
 }
 BaseSig::~BaseSig(){
-	delete[] raw_data;
+	delete raw_data;
 	cout << "Goodbye, BaseSig." << endl;
 }
-class ProcessedSig : public BaseSig{
-	
+//class ProcessedSig : public BaseSig{
+int main(void)
+{
+	BaseSig sig1;
+	sig1.printInfo();
+	return 1;
+}	
